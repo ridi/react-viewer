65a014075d9a35d27025a45d0626236511c93e5d
이재용 <lastiverse@gmail.com>
2017-09-13 18:08:41 +0900

    한글 작품 추가

--- watch.js
+++ watch.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const process = require('process');
 const webpackConfig = require('./webpack.config.js');
 
 const compiler = webpack(webpackConfig);
@@ -6,4 +7,5 @@ const watching = compiler.watch({}, (err, stats) => {
   err && console.error(err);
 });
 
+process.chdir('demo');
 require('./demo/watch.js');
