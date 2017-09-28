14c4fdd76c58eee3ff23952ab0d4a6a755bcb1e1
이재용 <lastiverse@gmail.com>
2017-09-28 18:10:30 +0900

    release-it build 커맨드 추가

--- release.js
+++ release.js
@@ -1,5 +1,9 @@
 const git = require('git-rev');
+const fs = require('fs-extra');
+const webpack = require('webpack');
 const npm = require('package-json');
+const viewerWebpackConfig = require('./webpack.config.js');
+const demoWebpackConfig = require('./demo/webpack.config.js');
 const { version, name } = require('./package.json');
 
 const exitWithErrorMsg = msg => {
@@ -7,13 +11,26 @@ const exitWithErrorMsg = msg => {
   process.exit(1);
 };
 
+const exec = cmd => new Promise((resolve, reject) => {
+  process.exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve();
+  });
+});
+
 const getGitBranch = () => new Promise((resolve, reject) => git.branch(branch => resolve(branch)));
+const gitCheckout = branch => exec(`git checkout ${branch}`);
+const gitCommitAndPush = commitMsg => exec(`git add . --all && git commit -m ${commitMsg} && git push`);
 
 const checkPreconditions = () => getGitBranch()
   .then(branch => {
     if (branch !== 'master') {
       exitWithErrorMsg('branch must be master');
     }
+    return npm(name);
   })
   .then(packageInfo => {
     if (version === packageInfo.version) {
@@ -21,16 +38,54 @@ const checkPreconditions = () => getGitBranch()
     }
   });
 
+const build = webpackConfig => new Promise((resolve, reject) => {
+  const compiler = webpack(webpackConfig);
+  compiler.run((err, stats) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(stats);
+  });
+});
+
+const renameDemoBundleWithVersion = ver =>
+  fs.rename('./demo/resources/js/index.js', `./demo/resources/js/${ver}.index.js`)
+    .then(() => {
+      const bundlesJson = require('./demo/resources/js/bundles.json');
+      bundlesJson.bundles.push(`${ver}.index.js`);
+      bundlesJson.latestVersion = ver;
+      return fs.writeJson('./demo/resources/js/bundles.json', bundlesJson);
+    });
+
 
 const args = process.argv.slice(2);
 const commands = {
   CHECK_PRECONDITIONS: 'check-preconditions',
+  BUILD: 'build',
+  RELEASED: 'released',
 };
 
 if (args == null || args.length === 0) {
   exitWithErrorMsg('args required for release.js');
 }
 
-if (args[0] === commands.CHECK_PRECONDITIONS) {
-  checkPreconditions();
-}
+const execArgs = arg => {
+  if (arg === commands.CHECK_PRECONDITIONS) {
+    return checkPreconditions();
+  } else if (arg === commands.BUILD) {
+    return build(viewerWebpackConfig)
+      .then(() => build(demoWebpackConfig))
+      .then(() => renameDemoBundleWithVersion(version));
+  } else if (arg === commands.RELEASED) {
+    return gitCheckout('gh-pages')
+      .then(() => gitCheckout('master ./demo/index.html'))
+      .then(() => gitCheckout('master ./demo/resources/js/*'))
+      .then(() => gitCommitAndPush(`version update ${version}`))
+      .then(() => gitCheckout('master'));
+  }
+  return Promise.reject('invalid args');
+};
+
+execArgs(args[0])
+  .catch(err => exitWithErrorMsg(err));
