dfdfde06c382df614f35bf75e9062edac1d11df6
kyungmi.k <kyungmi.k@ridi.com>
2018-05-02 16:36:07 +0900

    Add custom ESLint rule `'no-underscore-dangle': 0`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,5 @@
 module.exports = {
   'extends': '@ridi',
-  'globals': {
-    'jQuery': true,
-    '$': true,
-    'React': true,
-    'ReactDOM': true,
-    'ContentInfo': true,
-    'EpisodeInfo': true,
-    'EtcInfo': true,
-    'PageInitialize': true,
-    'jwplayer': true,
-    'AndroidBridge': true,
-    'CommonConfig': true,
-    'IMP': true,
-  },
   'env': {
     'browser': true,
     'node': true,
@@ -22,5 +8,6 @@ module.exports = {
     'import/prefer-default-export': 0,
     'react/require-default-props': 0,
     'react/forbid-prop-types': 0,
+    'no-underscore-dangle': 0,
   },
 };
